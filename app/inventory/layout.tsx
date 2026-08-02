bfb92e97c57893b34437005ee33bbb972c1413f8
Ayushi <ayushisainani@gmail.com>
2026-08-02 17:37:39 +0530

    Fix horizontal page scrollbar on inventory screens

--- app/inventory/layout.tsx
+++ app/inventory/layout.tsx
@@ -2,9 +2,9 @@
 import InventoryNav from "./InventoryNav";
 export default function InventoryLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex flex-col min-h-full w-full">
+    <div className="flex flex-col min-h-full w-full overflow-hidden">
       <InventoryNav />
-      <div className="flex-1 overflow-y-auto">{children}</div>
+      <div className="flex-1 overflow-y-auto overflow-x-hidden">{children}</div>
     </div>
   );
 }
